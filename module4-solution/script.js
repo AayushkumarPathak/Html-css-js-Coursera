f74108a947edaff876e696268e55ed6f850304db
AayushPathak <78433811+AayushkumarPathak@users.noreply.github.com>
2023-06-17 12:44:19 +0530

    Add files via upload

---
+++ module4-solution/script.js
@@ -0,0 +1,25 @@
+(function () {
+    var names = [
+      "Yaakov", "John", "Jen", "Jason", "Paul",
+      "Frank", "Larry", "Paula", "Laura", "Jim"
+    ];
+  
+    for (var i = 0; i < names.length; i++) {
+      var firstLetter = names[i].charAt(0).toLowerCase();
+  
+      if (firstLetter === 'j') {
+        byeSpeaker(names[i]);
+      } else {
+        helloSpeaker(names[i]);
+      }
+    }
+  
+    function byeSpeaker(name) {
+      console.log("Goodbye " + name);
+    }
+  
+    function helloSpeaker(name) {
+      console.log("Hello " + name);
+    }
+  })();
+  
