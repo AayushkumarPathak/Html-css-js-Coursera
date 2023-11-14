c3ac93e3f33a4a72d64ef025c12f35b269ffd646
AayushPathak <78433811+AayushkumarPathak@users.noreply.github.com>
2023-11-14 10:55:03 +0530

    Update SpeakHello.js

--- module4-solution/SpeakHello.js
+++ module4-solution/SpeakHello.js
@@ -1,12 +1,22 @@
+// (function(window) {
+//     var speakWord = "Hello";
+//     var helloSpeaker = {};
+  
+//     helloSpeaker.speak = function(name) {
+//       console.log(speakWord + " " + name);
+//     }
+  
+//     window.helloSpeaker = helloSpeaker;
+  
+//   })(window);
+
 (function(window) {
-    var speakWord = "Hello";
-    var helloSpeaker = {};
-  
-    helloSpeaker.speak = function(name) {
-      console.log(speakWord + " " + name);
-    }
-  
-    window.helloSpeaker = helloSpeaker;
-  
-  })(window);
+	var speakWord = "Hello";
+	var helloSpeaker = function (name) {
+		console.log(speakWord + " " + name);
+	}
+
+	window.helloSpeaker = helloSpeaker;
+
+})(window);
   
