b0fea5c0fdf7f579bb795fc0bb2a2ce073828443
AayushPathak <78433811+AayushkumarPathak@users.noreply.github.com>
2023-11-14 10:55:33 +0530

    Update SpeakGoodBye.js

--- module4-solution/SpeakGoodBye.js
+++ module4-solution/SpeakGoodBye.js
@@ -1,12 +1,22 @@
+// (function(window) {
+//     var speakWord = "Good Bye";
+//     var byeSpeaker = {};
+  
+//     byeSpeaker.speak = function(name) {
+//       console.log(speakWord + " " + name);
+//     }
+  
+//     window.byeSpeaker = byeSpeaker;
+  
+//   })(window);
+
+
 (function(window) {
-    var speakWord = "Good Bye";
-    var byeSpeaker = {};
-  
-    byeSpeaker.speak = function(name) {
-      console.log(speakWord + " " + name);
-    }
-  
-    window.byeSpeaker = byeSpeaker;
-  
-  })(window);
-  
+	var speakWord = "Good Bye";
+	var byeSpeaker = function (name) {
+  		console.log(speakWord + " " + name);
+	}
+	
+	window.byeSpeaker = byeSpeaker;
+
+})(window);
